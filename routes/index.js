3d35188dc47c3129da271ba183c16150bb7590ca
אלישבע מנס <212127567@mbydomain>
2021-06-03 18:51:35 +0300

    postLesson

--- routes/index.js
+++ routes/index.js
@@ -86,6 +86,25 @@ router.post("/signup", function (req, res) {
   });
 });
 
+  
+router.post("/postLesson", function (req, res) {
+  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+  const { numLesson, lessonName,file,date,notes,time } = req.body; //Adress, phone ....
+  //Validations.
+  //Check if user exists
+  MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("mySchoolDB");
+    var myobj = { numLesson, lessonName,file,date,notes,time };
+    dbo.collection("lessons").insertOne(myobj, function (err, res) {
+      if (err) throw err;
+      console.log("1 document inserted");
+      db.close();
+    });
+    const token = generateAccessToken(user);
+    console.log("token", token);
+    return res.json({ token }).send();
+  });
+});
 
 module.exports = router;
-
