fa86e583d9f4f8872e52bf6bd72a897b74677d35
Your Name <you@example.com>
2021-07-06 01:53:43 +0300

    elish!!

--- controllers/previousLessons.js
+++ controllers/previousLessons.js
@@ -12,15 +12,22 @@ class PreviousLessons {
 
     previousLessons = (req, res) => {
         res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+        const { id } = req.query;
         MongoClient.connect(url, async function (err, db) {
             if (err)
                 return res.status(500).send(err);
             var dbo = db.db("mySchoolDB");
             try{
-            let resultStudent =await  dbo.collection("student").findone({student:req.params.student});
+     debugger
+     var query = { id};
+     let resultStudent
+     resultStudent = await dbo.collection("student").findOne(query)
+
+            // let resultStudent =await  dbo.collection("student").find({student:req.params.id});
             console.log(resultStudent);
-            let resultTeacher =await  dbo.collection("teacher").findone({subject:resultStudent.subject});
-            let result =await  dbo.collection("lessons").find({teacher:resultTeacher.email}).toArray();
+            // let resultTeacher =await  dbo.collection("teacher").findone({subject:resultStudent.subject});
+            let result =await  dbo.collection("lessons").find({subject:resultStudent.subject}).toArray();
+            console.log(result);
             db.close();
             return res.status(200).json(result);
             }catch(error){
